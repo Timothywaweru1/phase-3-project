1422cbef3f2809fa1edb095f4fd3580c22c9aa09
Timothy <timothy.waweru1@student.moringaschool.com>
2025-05-28 17:42:20 +0300

    added debug

---
+++ debug.py
@@ -0,0 +1,12 @@
+from controllers import controller
+
+def main():
+    print("Students enrolled in courses:")
+
+    student_id = 1  
+    courses = controller.get_courses_for_student(student_id)
+    for course in courses:
+        print(f"Student {student_id} enrolled in Course: {course}")
+
+if __name__ == "__main__":
+    main()
